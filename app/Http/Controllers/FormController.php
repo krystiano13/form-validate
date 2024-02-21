e86fc45decf0084c7b03b82b0fcc3e6076888f8e
krystiano13 <crystianotv@gmail.com>
2024-02-21 17:52:36 +0100

    create php function for server-side validation

--- app/Http/Controllers/FormController.php
+++ app/Http/Controllers/FormController.php
@@ -3,10 +3,34 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class FormController extends Controller
 {
     public function index() {
         return view('app');
+    }
+
+    public function store(Request $request) {
+        $validator = Validator::make($request -> all(),[
+            'name' => ['required', 'max:100'],
+            'email' => ['required', 'email'],
+            'text' => ['required', 'max:500'],
+            'phone' => ['required', 'numeric'],
+            'file' => ['file', 'size:5120', 'mimes:jpg, pdf']
+        ]);
+
+        if($validator -> fails()) {
+            return response([
+                'status' => false,
+                'message' => 'Validation Error',
+                'errors' => $validator -> errors()
+            ],403);
+        }
+
+        return response([
+            'status' => true,
+            'message' => 'Message Sent'
+        ], 200);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,4 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\FormController;
 
 Route::get('/', [FormController::class, 'index']);
+Route::post('/send', [FormController::class, 'store']);
